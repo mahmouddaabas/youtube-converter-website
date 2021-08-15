72662c55676e85dedb88dc6b28578379a74efd3d
Mahmoud Daabas <mahedaabas8@gmail.com>
2021-08-15 04:27:25 +0200

    Backend update
    - Now directly converts the video in the ExpressJS request and downloads it.
    
    - The code in download.js is no longer used.

--- backend/javascript/server.js
+++ backend/javascript/server.js
@@ -1,24 +1,34 @@
 const express = require('express');
 const app = express();
-const convert = require('./download');
 const cors = require('cors');
 const port = process.env.PORT || 3001;
+const ytdl = require('ytdl-core');
+
 
 app.use(cors());
 
 app.get('/convert/:url/:format', async (request, response) => {
     console.log('Converting video.')
     const url = request.params.url;
-    const format = request.params.format;
-    //console.log(url, format)
+    const selected_format = request.params.format;
 
     dl_url = 'https://www.youtube.com/watch?' + url
-    //console.log(dl_url)
 
-    let data = await convert.video_information(dl_url, format);
-    //console.log(data)
-    response.send("Converting video, please wait...")
-    response.send(data)
+    ytdl.getInfo(dl_url).then(info => {
+        
+    videoname = info.videoDetails.title; //grab video title
+    for(var i = 0; i < 5; i++){ //replace | with -
+        videoname = videoname.replace("|", '-')
+        }
+        videoname = videoname.replace(/[^a-z0-9 ,.?!]/ig, '') //remove all illegal characters (ex arabic alphabet)
+        //console.log(videoname + selected_format)
+
+    response.header("Content-Disposition", 'attachment;\  filename="' + videoname + selected_format)
+
+    ytdl(dl_url ,{format: selected_format})
+
+    .pipe(response)
+    })
 });
 
 app.listen(port, () => {
