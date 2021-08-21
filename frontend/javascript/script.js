7ab22c29610ce1660fa9ea54561fe538d1c66389
Mahmoud Daabas <mahedaabas8@gmail.com>
2021-08-21 20:11:29 +0200

    Front end update
    - Update to be compatible with the python backend that was implemented.

--- frontend/javascript/script.js
+++ frontend/javascript/script.js
@@ -1,18 +1,18 @@
 $(document).ready(() => {
 
     var format;
-    var quality;
+    var quality = "high";
 
     $(".mp3").on("click", () => {
         $(".mp3").css('background-color','green');
         $(".mp4").css('background-color','white');
-        format = ".mp3"
+        format = "mp3"
     })
 
     $(".mp4").on("click", () => {
         $(".mp4").css('background-color','green');
         $(".mp3").css('background-color','white');
-        format = ".mp4"
+        format = "mp4"
     })
 
     $(".quality-select-box").change(() => {
@@ -21,15 +21,14 @@ $(document).ready(() => {
 
     $(".convert-btn").on("click", () => {
         if(format != undefined) { //check if a format has been selected
-            split_url($(".url-input").val()).then(url_response => {
-                fetch('127.0.0.1/3001/convert/' + url_response + "/" + format + "/" + quality)
+                fetch('https://youtube-converter-web-py.herokuapp.com/download?url=' + $(".url-input").val() + "&format=" + format + "&quality=" + quality)
                 .then(response => {
+                    alert(response.url)
                     var url = response.url; //get the url from the response
                     var a = document.createElement("a") //create an a element
                     a.href = url; //set the url to the a documents href
                     a.click(); //click on the a document to trigger a browser download
                     a.remove(); //remove the a element once its done
-                })
             })
         }
         else {
